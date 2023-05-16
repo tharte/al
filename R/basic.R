ce8fc5cd4649423b363e6cf8ff0d79edc4995c60
Thomas P. Harte <tharte@cantab.net>
2023-05-16 22:45:42 +0000

    indent: malformed

---
+++ R/basic.R
@@ -0,0 +1,14 @@
+#' Identity Combinator
+#'
+#' \lambda x.x
+#' @param x input
+#' @return x its input
+#' @author Thomas P. Harte
+#' @export
+#' @examples
+#' ID(\(x) x)        # identity of the identity is the identity
+#' ID(\(a) a)        # α-equivalence 
+#' ID(\(x) \(y) x)   # identity of Boolean truth is Boolean truth
+#' ID(\(x) \(y) y)   # identity of Boolean falsity is Boolean falsity
+#' foo_function()
+`ID`<- \(x) x
