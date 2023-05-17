d1fff0536f106bf0821eabb3bc846db420cbb6d5
Thomas P. Harte <tharte@cantab.net>
2023-05-17 00:02:18 +0000

    BUGFIX: a -> .a

--- tests/testthat/test_basic.R
+++ tests/testthat/test_basic.R
@@ -3,7 +3,7 @@ test_that("identity combinator works", {
   .true<-  \(x) \(y) x
   .false<- \(x) \(y) y
 
-  expect_identical(ID(a), a)             # identity of the identity is the identity
+  expect_identical(ID(.a), .a)           # identity of the identity is the identity
   expect_identical(ID(.true), .true)     # identity of Boolean truth is Boolean truth
   expect_identical(ID(.false), .false)   # identity of Boolean falsity is Boolean falsity
 })
