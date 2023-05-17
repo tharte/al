7d15bd304f3db806ebce2cbbab1f6be01ddbda6e
Thomas P. Harte <tharte@cantab.net>
2023-05-17 00:02:39 +0000

    test AND

--- tests/testthat/test_boolean.R
+++ tests/testthat/test_boolean.R
@@ -1,7 +1,12 @@
 test_that("Boolean primitives work", {
-  .true<-  \(x) \(y) x
-  .false<- \(x) \(y) y
-
   expect_equal(true(1)(2), 1)          
   expect_equal(false(1)(2), 2)          
+})
+
+# AND: truth table
+test_that("Boolean AND works", {
+  expect_identical(and(false)(false), false)
+  expect_identical(and(false)(true),  false)
+  expect_identical(and(true)(false),  false)
+  expect_identical(and(true)(true),   true)
 })
