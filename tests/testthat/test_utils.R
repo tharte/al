1a93f1767366746632a15930bade670762d0e02d
Thomas P. Harte <tharte@cantab.net>
2023-05-17 00:11:27 +0000

    uncomment

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,6 +1,6 @@
 test_that(".to.integer works", {
-  #expect_equal(true(1)(2), 1)          
-  #expect_equal(false(1)(2), 2)          
+  expect_equal(true(1)(2), 1)          
+  expect_equal(false(1)(2), 2)          
 })
 
 test_that(".to.logical works", {
